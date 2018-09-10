d363f1e650b0a94f26c099d3cc4e796680fe9751
Hongjiang Zhang <honzhan@microsoft.com>
2018-09-10 16:28:13 +0800

    init project

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.SignalR.Samples.Serverless;
+using Microsoft.Extensions.CommandLineUtils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Transceiver
+{
+    class Program
+    {
+        private const int TRANSMIT_DELAY_MS = 100;
+        private static CancellationTokenSource cancellationToken = new CancellationTokenSource();
+        private static ServerHandler _server;
+        private static Dictionary<string, string> _settings;
+        private static string[] _hubIds;
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Specify <client>|<server>");
+                Console.WriteLine("  <client> client-count: default client count is 1000");
+                Console.WriteLine("  <server> target-count <server-count>: default server count is 2");
+                return;
+            }
+            var settingsPath = Path.Combine(Environment.CurrentDirectory, "local.settings.json");
+            _settings = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(settingsPath));
+
+            var hubIdsPath = Path.Combine(Environment.CurrentDirectory, "HubIds.csv");
+            _hubIds = File.ReadAllLines(hubIdsPath)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .ToArray();
+            if (args[0].Equals("client"))
+            {
+                var clientCount = 1000;
+                if (args.Length == 2)
+                {
+                    clientCount = int.Parse(args[1]);
+                }
+                Task.Run(async () =>
+                {
+                    var counter = new Counter();
+                    var client = new ClientHandler(_settings["SignalRServiceConnectionString"], ServiceUtils.HubName, clientCount, counter);
+                    counter.StartPrint();
+                    await client.StartAsync();
+                    Console.WriteLine("Client started...");
+                    Console.ReadLine();
+                    await client.DisposeAsync();
+                }).Wait();
+            }
+            else
+            {
+                var targetClient = 0;
+                if (args.Length < 2)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    targetClient = int.Parse(args[1]);
+                }
+
+                var threadCount = 2;
+                if (args.Length == 3)
+                {
+                    threadCount = int.Parse(args[2]);
+                }
+                _server = new ServerHandler(_settings["SignalRServiceConnectionString"], _hubIds);
+                // increase the concurrent connection limit.
+                ServicePointManager.DefaultConnectionLimit = threadCount;
+                var txThreads = Enumerable.Range(0, threadCount)
+                    .Select(_ => new Thread(() => TransmitEvents(targetClient)))
+                    .ToList();
+                foreach (var txThread in txThreads)
+                    txThread.Start();
+
+                Thread.Sleep(TimeSpan.FromMinutes(5));
+                cancellationToken.Cancel();
+            }
+        }
+
+        static async void TransmitEvents(int targetClient)
+        {
+            while (!cancellationToken.Token.IsCancellationRequested)
+            {
+                await _server.SendToUsers(ServiceUtils.HubName, targetClient);
+                await Task.Delay(TRANSMIT_DELAY_MS);
+            }
+        }
+    }
+}
---
+++ ServerHandler.cs
@@ -0,0 +1,130 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Newtonsoft.Json;
+using Shared;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.SignalR.Samples.Serverless
+{
+    public class ServerHandler : IDisposable
+    {
+        private HttpClient _client;
+        private readonly string _serverName;
+
+        private readonly ServiceUtils _serviceUtils;
+
+        private readonly string[] _hubNames;
+
+        private readonly string _endpoint;
+
+        private string _target = ServiceUtils.MethodName;
+
+        private bool _disposed = false;
+
+        public ServerHandler(string connectionString, string[] hubNames)
+        {
+            _client = new HttpClient();
+            _serverName = GenerateServerName();
+            _serviceUtils = new ServiceUtils(connectionString);
+            _hubNames = hubNames;
+            _endpoint = _serviceUtils.Endpoint;
+        }
+
+        public async Task SendToUsers(string hubName, int maxUser)
+        {
+            var random = new Random();
+            for (var i = 0; i < maxUser; i++)
+            {
+                var url = GetSendToUserUrl(hubName, $"{ServiceUtils.ClientUserIdPrefix}{i}");
+                var request = new HttpRequestMessage(HttpMethod.Post, GetUrl(url));
+
+                request.Headers.Authorization =
+                    new AuthenticationHeaderValue("Bearer", _serviceUtils.GenerateAccessToken(url, _serverName));
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var payloadRequest = new PayloadMessage
+                {
+                    Target = _target,
+                    Arguments = new[]
+                    {
+                    _serverName,
+                    $"{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}",
+                }
+                };
+                request.Content = new StringContent(JsonConvert.SerializeObject(payloadRequest), Encoding.UTF8, "application/json");
+                var response = await _client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+
+        public async Task BroadcastMessage()
+        {
+            var random = new Random();
+            var hubName = _hubNames[random.Next(_hubNames.Length)];
+            var url = GetBroadcastUrl(hubName);
+            var request = new HttpRequestMessage(HttpMethod.Post, GetUrl(url));
+
+            request.Headers.Authorization =
+                new AuthenticationHeaderValue("Bearer", _serviceUtils.GenerateAccessToken(url, _serverName));
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var payloadRequest = new PayloadMessage
+            {
+                Target = _target,
+                Arguments = new[]
+                {
+                    _serverName,
+                    $"{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}",
+                }
+            };
+            request.Content = new StringContent(JsonConvert.SerializeObject(payloadRequest), Encoding.UTF8, "application/json");
+            var response = await _client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+        }
+
+        private Uri GetUrl(string baseUrl)
+        {
+            return new UriBuilder(baseUrl).Uri;
+        }
+
+        private string GetBroadcastUrl(string hubName)
+        {
+            return $"{GetBaseUrl(hubName)}";
+        }
+
+        private string GetSendToUserUrl(string hubName, string userId)
+        {
+            return $"{GetBaseUrl(hubName)}/user/{userId}";
+        }
+
+        private string GetBaseUrl(string hubName)
+        {
+            return $"{_endpoint}:5002/api/v1-preview/hub/{hubName.ToLower()}";
+        }
+
+        private string GenerateServerName()
+        {
+            return $"{Environment.MachineName}_{Guid.NewGuid():N}";
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _client.Dispose();
+            }
+        }
+
+        private class PayloadMessage
+        {
+            public string Target { get; set; }
+
+            public object[] Arguments { get; set; }
+        }
+    }
+
+    
+}
